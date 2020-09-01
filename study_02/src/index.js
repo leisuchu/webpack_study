7930b6c5b6756a81d702fcfbca5eaadf0884a9be
leisuchu <363126838@qq.com>
2020-09-01 16:45:35 +0800

    loaders 加载器测试

---
+++ study_02/src/index.js
@@ -0,0 +1,3 @@
+import './index.css'
+import './index.less'
+console.log('index.js 被加载');
---
+++ study_02/webpack.config.js
@@ -0,0 +1,41 @@
+const {resolve} = require('path') 
+module.exports = {
+    // 入口文件路径
+    entry: './src/index.js',
+    // 输出打包文件路径
+    output: {
+        filename: 'lei.js',
+        path: resolve(__dirname, 'build'),
+    },
+    // loaders配置 module不加s
+    module: {
+        rules: [
+            // css loader
+            {
+                test:/\.css$/,
+                // 执行顺序：从右往左，或者说从下往上
+                use:[
+                    // 创建style标签
+                    'style-loader',
+                    // 以字符串的形式将css载入js，生成commonjs模块
+                    'css-loader'
+                ]
+            },
+            // less loader
+            {
+                test:/\.less/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    // plugins配置
+    plugins: [
+
+    ],
+    // 打包模式，开发/生产（production）
+    mode: 'development'
+}
